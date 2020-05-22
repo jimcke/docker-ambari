8fbfbd52182c7b675968575608bbd1ca2617de45
Alexandru Georgescu <alexandru.g@trgint.com>
2020-05-22 16:43:48 +0300

    Wip upgrade newer version of ambari

---
+++ dev-cluster.sh
@@ -0,0 +1,10 @@
+
+
+
+
+source ambari-functions
+amb-settings
+amb-start-cluster 2
+#sleep 150
+#curl -X POST -H "Content-Type: application/json" -H "X-Requested-By: ambari" -u admin:admin -d @blues/blueprint.json http://172.17.0.3:8080/api/v1/blueprints/trgcluster
+#curl -X POST -H "Content-Type: application/json" -H "X-Requested-By: ambari" -u admin:admin -d @blues/clustertemplate.json http://172.17.0.3:8080/api/v1/clusters/trgcluster
